30e4aa822c711ecc321f63fbc8e7da40c38f8030
yeyushengfan258 <258977548@qq.com>
2021-05-03 00:48:46 +0800

    update

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ fi
 SRC_DIR=$(cd $(dirname $0) && pwd)
 
 THEME_NAME=Lyra
-THEME_VARIANTS=('' '-red' '-orange' '-purple' )
+THEME_VARIANTS=('' '-red' '-orange' '-purple' '-pink' )
 COLOR_VARIANTS=('' '-dark')
 
 usage() {
